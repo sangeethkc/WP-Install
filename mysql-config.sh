01e08894e970c02d3e2e03e1ef30bd2a4ca8b21c
sangeethkc <keyceesangi@gmail.com>
2025-01-05 03:36:00 +0530

    mysql user change

--- mysql-config.sh
+++ mysql-config.sh
@@ -15,7 +15,7 @@ if ! sudo systemctl is-active mysql &> /dev/null; then
 fi
 
 # Secure MySQL installation (automate the 'mysql_secure_installation' steps)
-sudo mysql -e "UPDATE mysql.user SET Password=PASSWORD('$DB_PASS') WHERE User='wordpress_user';"
+sudo mysql -e "UPDATE mysql.user SET Password=PASSWORD('$DB_PASS') WHERE User='root';"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DROP DATABASE IF EXISTS test;"
 sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 # Variables
 DB_NAME="wordpress_db"
-DB_USER="wordpress_user"
-DB_PASS="securepassword"
+DB_USER="root"
+DB_PASS="root123"
 WP_DIR="/var/www/html/wordpress"
 APACHE_CONF="/etc/apache2/sites-available/wordpress.conf"
 DOMAIN="wp.sangeeth.cloud"  # Replace with your domain or IP
