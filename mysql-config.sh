843d73b806640af03b9d66a21893c71ff1834526
sangeethkc <keyceesangi@gmail.com>
2025-01-05 04:32:37 +0530

    entire code change from claude

--- mysql-config.sh
+++ mysql-config.sh
@@ -16,45 +16,11 @@ fi
 
 echo "Starting WordPress database setup..."
 
-read -p "MySQL server superior username. E.g. root: " DB_USER
-read -p "MySQL server superior password: " DB_PASS
+sudo mysql_secure_installation
 
-if [ -z "$DB_USER" ]
-then
-  DB_USER="root"
-fi
-
-if [ -z "$DB_PASS" ]
-then
-  DB_PASS="root123"
-fi
-
-
-mysql -h $DB_HOST -u $DB_USER -p$DB_PASS <<SHELL
-  create user '$DB_USER'@'$USER_HOST' identified by '$DB_PASS';
-  create database if not exists $DB_NAME;
-  grant alter,create,delete,drop,index,lock tables,insert,select,update on $DB_NAME.* to '$DB_USER'@'$USER_HOST';
-  flush privileges;
-  quit
-SHELL
-
-# Database created.
-echo -e "\033[32mSuccess\033[0m: Database has been created successfully!"
-
-
-# sudo mysql -u root -e "SELECT user, host, plugin FROM mysql.user WHERE user = 'root';"
-
-# sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root123';"
-# sudo mysql -u root -e "FLUSH PRIVILEGES;"
-
-# sudo mysql -u root -p'root123' -e "CREATE DATABASE $DB_NAME;"
-
-
-# # Secure MySQL installation (automate the 'mysql_secure_installation' steps)
-# sudo mysql -e "UPDATE mysql.user SET Password=PASSWORD('$DB_PASS') WHERE User='root';"
-# sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
-# sudo mysql -e "DROP DATABASE IF EXISTS test;"
-# sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-# sudo mysql -e "FLUSH PRIVILEGES;"
-
-# echo "MySQL configuration completed successfully."
+sudo mysql -u root -p$DB_PASS <<EOF
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'$USER_HOST' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$USER_HOST';
+FLUSH PRIVILEGES;
+EOF
--- wp-install.sh
+++ wp-install.sh
@@ -1,15 +1,78 @@
 #!/bin/bash
 
-source variables.sh
+# Exit on error
+set -e
 
-echo "WordPress installation and configuration completed."
-echo "Please complete the installation through the web interface by visiting http://$DOMAIN"
+# Update system
+apt-get update
+apt-get upgrade -y
 
-# do a curl to the domain to check if the installation is successful
-curl -I http://$DOMAIN
+# Install LAMP stack
+apt-get install -y apache2 mysql-server php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
 
-if [ $? -eq 0 ]; then
-    echo "WordPress installation successful"
-else
-    echo "WordPress installation failed"
-fi
+# Start services
+systemctl start apache2
+systemctl start mysql
+
+# Configure MySQL
+mysql_secure_installation <<EOF
+
+y
+password123
+password123
+y
+y
+y
+y
+EOF
+
+# Create WordPress database
+mysql -u root -ppassword123 <<EOF
+CREATE DATABASE wordpress;
+CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'wordpress123';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+# Download and configure WordPress
+cd /var/www/html
+rm -rf index.html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* .
+rm -rf wordpress latest.tar.gz
+
+# Set permissions
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+# Create wp-config.php
+cp wp-config-sample.php wp-config.php
+sed -i "s/database_name_here/wordpress/" wp-config.php
+sed -i "s/username_here/wordpressuser/" wp-config.php
+sed -i "s/password_here/wordpress123/" wp-config.php
+
+# Generate and set security keys
+SECURITY_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+sed -i "/define( 'AUTH_KEY'/,/define( 'NONCE_SALT'/{/define( 'AUTH_KEY'/i\\$SECURITY_KEYS" wp-config.php
+sed -i "/define( 'AUTH_KEY'/,/define( 'NONCE_SALT'/d" wp-config.php
+
+# Configure Apache
+cat > /etc/apache2/sites-available/wordpress.conf <<EOF
+<VirtualHost *:80>
+    DocumentRoot /var/www/html
+    ServerName localhost
+    
+    <Directory /var/www/html>
+        AllowOverride All
+    </Directory>
+</VirtualHost>
+EOF
+
+# Enable Apache modules and configuration
+a2enmod rewrite
+a2ensite wordpress
+a2dissite 000-default
+systemctl restart apache2
+
+echo "WordPress installation complete. Access http://localhost to complete setup."
